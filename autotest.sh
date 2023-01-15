ecb0afbc97bd2062bdaa9a8652f95fef058e4b2a
JeyDot-dev <72892603+JeyDot-dev@users.noreply.github.com>
2023-01-15 11:16:33 +0100

    ahah 4th try

--- autotest.sh
+++ autotest.sh
@@ -1,73 +1,73 @@
 echo  "\n\033[0;31m*****test one : BUFFER_SIZE = 1******"
 echo  "\n\033[0m			----- file 1 -----"
-gcc -fsanitize=address -Wextra -Werror -Wall -D BUFFER_SIZE=1 get_next_line.c tst.c 
+gcc -Wextra -Werror -Wall -D BUFFER_SIZE=1 get_next_line.c tst.c 
 ./a.out | cat -e
 echo  "\n			----- file 2 -----"
-gcc -fsanitize=address -Wextra -Werror -Wall -D BUFFER_SIZE=1 get_next_line.c tst2.c 
+gcc -Wextra -Werror -Wall -D BUFFER_SIZE=1 get_next_line.c tst2.c 
 ./a.out | cat -e
 echo  "\n\033[0;34m=====end of test one====="
 echo  "\n\033[0;31m*****test two : BUFFER_SIZE = 10******"
 echo  "\n\033[0m			----- file 1 -----"
-gcc -fsanitize=address -Wextra -Werror -Wall -D BUFFER_SIZE=10 get_next_line.c tst.c 
+gcc -Wextra -Werror -Wall -D BUFFER_SIZE=10 get_next_line.c tst.c 
 ./a.out | cat -e
 echo  "\n			----- file 2 -----"
-gcc -fsanitize=address -Wextra -Werror -Wall -D BUFFER_SIZE=10 get_next_line.c tst2.c 
+gcc -Wextra -Werror -Wall -D BUFFER_SIZE=10 get_next_line.c tst2.c 
 ./a.out | cat -e
 echo  "\n\033[0;34m=====end of test two====="
 echo  "\n\033[0;31m*****test three : BUFFER_SIZE = 17******"
 echo  "\n\033[0m			----- file 1 -----"
-gcc -fsanitize=address -Wextra -Werror -Wall -D BUFFER_SIZE=17 get_next_line.c tst.c 
+gcc -Wextra -Werror -Wall -D BUFFER_SIZE=17 get_next_line.c tst.c 
 ./a.out | cat -e
 echo  "\n			----- file 2 -----"
-gcc -fsanitize=address -Wextra -Werror -Wall -D BUFFER_SIZE=17 get_next_line.c tst2.c 
+gcc -Wextra -Werror -Wall -D BUFFER_SIZE=17 get_next_line.c tst2.c 
 ./a.out | cat -e
 echo  "\n\033[0;34m=====end of test three====="
 echo  "\n\033[0;31m*****test four : BUFFER_SIZE = 42******"
 echo  "\n\033[0m			----- file 1 -----"
-gcc -fsanitize=address -Wextra -Werror -Wall -D BUFFER_SIZE=42 get_next_line.c tst.c 
+gcc -Wextra -Werror -Wall -D BUFFER_SIZE=42 get_next_line.c tst.c 
 ./a.out | cat -e
 echo  "\n			----- file 2 -----"
-gcc -fsanitize=address -Wextra -Werror -Wall -D BUFFER_SIZE=42 get_next_line.c tst2.c 
+gcc -Wextra -Werror -Wall -D BUFFER_SIZE=42 get_next_line.c tst2.c 
 ./a.out | cat -e
 echo  "\n\033[0;34m=====end of test four====="
 echo  "\n\033[0;31m*****test five : BUFFER_SIZE = 77******"
 echo  "\n\033[0m			----- file 1 -----"
-gcc -fsanitize=address -Wextra -Werror -Wall -D BUFFER_SIZE=77 get_next_line.c tst.c 
+gcc -Wextra -Werror -Wall -D BUFFER_SIZE=77 get_next_line.c tst.c 
 ./a.out | cat -e
 echo  "\n			----- file 2 -----"
-gcc -fsanitize=address -Wextra -Werror -Wall -D BUFFER_SIZE=77 get_next_line.c tst2.c 
+gcc -Wextra -Werror -Wall -D BUFFER_SIZE=77 get_next_line.c tst2.c 
 ./a.out | cat -e
 echo  "\n\033[0;34m=====end of test five====="
 echo  "\n\033[0;31m*****test six : BUFFER_SIZE = 144******"
 echo  "\n\033[0m			----- file 1 -----"
-gcc -fsanitize=address -Wextra -Werror -Wall -D BUFFER_SIZE=144 get_next_line.c tst.c 
+gcc -Wextra -Werror -Wall -D BUFFER_SIZE=144 get_next_line.c tst.c 
 ./a.out | cat -e
 echo  "\n			----- file 2 -----"
-gcc -fsanitize=address -Wextra -Werror -Wall -D BUFFER_SIZE=144 get_next_line.c tst2.c 
+gcc -Wextra -Werror -Wall -D BUFFER_SIZE=144 get_next_line.c tst2.c 
 ./a.out | cat -e
 echo  "\n\033[0;34m=====end of test six====="
 echo  "\n\033[0;31m*****test seven : BUFFER_SIZE = 1'000******"
 echo  "\n\033[0m			----- file 1 -----"
-gcc -fsanitize=address -Wextra -Werror -Wall -D BUFFER_SIZE=1000 get_next_line.c tst.c 
+gcc -Wextra -Werror -Wall -D BUFFER_SIZE=1000 get_next_line.c tst.c 
 ./a.out | cat -e
 echo  "\n			----- file 2 -----"
-gcc -fsanitize=address -Wextra -Werror -Wall -D BUFFER_SIZE=1000 get_next_line.c tst2.c 
+gcc -Wextra -Werror -Wall -D BUFFER_SIZE=1000 get_next_line.c tst2.c 
 ./a.out | cat -e
 echo  "\n\033[0;34m=====end of test seven====="
 echo  "\n\033[0;31m*****test eight : BUFFER_SIZE = 100'000******"
 echo  "\n\033[0m			----- file 1 -----"
-gcc -fsanitize=address -Wextra -Werror -Wall -D BUFFER_SIZE=100000 get_next_line.c tst.c 
+gcc -Wextra -Werror -Wall -D BUFFER_SIZE=100000 get_next_line.c tst.c 
 ./a.out | cat -e
 echo  "\n			----- file 2 -----"
-gcc -fsanitize=address -Wextra -Werror -Wall -D BUFFER_SIZE=100000 get_next_line.c tst2.c 
+gcc -Wextra -Werror -Wall -D BUFFER_SIZE=100000 get_next_line.c tst2.c 
 ./a.out | cat -e
 echo  "\n\033[0;34m=====end of test eight====="
 echo  "\n\033[0;31m*****test nine : BUFFER_SIZE = 1'000'000******"
 echo  "\n\033[0m			----- file 1 -----"
-gcc -fsanitize=address -Wextra -Werror -Wall -D BUFFER_SIZE=1000000 get_next_line.c tst.c 
+gcc -Wextra -Werror -Wall -D BUFFER_SIZE=1000000 get_next_line.c tst.c 
 ./a.out | cat -e
 echo  "\n			----- file 2 -----"
-gcc -fsanitize=address -Wextra -Werror -Wall -D BUFFER_SIZE=1000000 get_next_line.c tst2.c 
+gcc -Wextra -Werror -Wall -D BUFFER_SIZE=1000000 get_next_line.c tst2.c 
 ./a.out | cat -e
 echo "\n\033[0;34m=====end of test nine====="
 echo "END OF TESTS"
--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jsousa-a <jsousa-a@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 20:23:20 by jsousa-a          #+#    #+#             */
-/*   Updated: 2023/01/14 18:18:58 by jsousa-a         ###   ########.fr       */
+/*   Updated: 2023/01/15 11:14:53 by jsousa-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
@@ -21,7 +21,7 @@ char	*g_calloc(int ct)
 	char	*ptr;
 	int		i;
 
-	if (ct < 2)
+	if (ct < 1)
 		return (NULL);
 	ptr = malloc(ct);
 	if (!ptr)
@@ -36,41 +36,14 @@ int	g_strlen(char *str)
 	int	i;
 
 	i = 0;
-	while (str && str[i])
+	while (str[i])
 		i++;
 	return (i);
 }
-/*char *g_strdup(char *str)
-{
-	char	*cpy;
-	int		i;
-	int		len;
-
-//									printf("---strdup STR = %s\n", str);
-	if (!str)
-		return (NULL);
-	i = 0;
-	len = g_strlen(str);
-	if (len < 1)
-		return (NULL);
-	cpy = g_calloc(sizeof(char) * (len + 1));
-	if (!cpy)
-		return (NULL);
-	while (str[i])
-	{
-		cpy[i] = str[i];
-		i++;
-	}
-	free(str);
-//									printf("strdup CPY = %s\n", cpy);
-	return (cpy);
-}*/
 int	g_check_n(char *str)
 {
 	int	i;
 
-	if (!str)
-		return (-1);
 	i = 0;
 	while (str[i] != '\n' && str[i])
 		i++;
@@ -78,141 +51,14 @@ int	g_check_n(char *str)
 		return (i);
 	return (-1);
 }
-char *g_cat(char *sttc_str, char *buffer, int read_ct)
-{
-	int		i[2];
-	char	*newstr;
-
-	if (!buffer || read_ct < 1)
-		return (sttc_str);
-	i[0] = 0;
-	i[1] = 0;
-	newstr = g_calloc(sizeof(char) * g_strlen(sttc_str) + read_ct + 1);
-	if (!newstr)
-		return (NULL);
-	while (sttc_str && sttc_str[i[0]])
-	{
-		newstr[i[0]] = sttc_str[i[0]];
-		i[0]++;
-	}
-	while (i[1] < read_ct)
-		newstr[i[0]++] = buffer[i[1]++];
-	if (sttc_str)
-		sttc_str = g_free(sttc_str);
-//										printf("******G_CAT RESULT : %s******\n", newstr);
-	return (newstr);
-}
-char	*g_cut_from_n(char *str)
-{
-	char	*temp;
-	int	i;
-	int	j;
-	int len;
-
-//									printf("^^^start of g_cut_from^^^\n");
-	if (!str)
-		return (NULL);
-	i = g_check_n(str) + 1;
-	len = g_strlen(str);
-	if (i < len)
-		temp = g_calloc(sizeof(char) * (len + 1 - i));
-	else
-		return (NULL);
-	if (!temp)
-		return (NULL);
-	j = 0;
-	while (i < len)
-		temp[j++] = str[i++];
-//									printf("str = %s\n", str);
-//									printf("test\n");
-//									write(1, &temp[0], 1);
-//								printf("g_cut from before free\n");
-	return (temp);
-}
-char	*g_cut_to_n(char *str)
-{
-	char	*newstr;
-	int		i[2];
-//										printf("---start of g_cut_to---\n");
-//										printf("---sttc value at g_cut_to : %s---\n", str);
-	if (!str || !*str)
-		return (NULL);
-	i[0] = g_check_n(str);
-	if (i[0] < g_strlen(str))
-		newstr = g_calloc(sizeof(char) * (i[0] + 2));
-	else
-		return (NULL);
-	if (!newstr)
-		return (NULL);
-	i[1] = -1;
-	while (++i[1] <= i[0])
-		newstr[i[1]] = str[i[1]];
-//	free(str);
-//										printf("---g_cut_to result : %s\n", newstr);
-	return (newstr);
-}
-char	*get_line(int fd, char *sttc_str)
-{
-	char		*buffer;
-	ssize_t		read_ct;
-
-	read_ct = BUFFER_SIZE;
-	buffer = g_calloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!buffer)
-		return (NULL);
-	while (read_ct == BUFFER_SIZE && g_check_n(sttc_str) < 0)
-	{
-		read_ct = read(fd, buffer, BUFFER_SIZE);
-		if (read_ct == -1)
-		{
-			free(buffer);
-			free(sttc_str);
-			return (NULL);
-		}
-		if (read_ct > 0)
-			sttc_str = g_cat(sttc_str, buffer, read_ct);
-	}
-	free (buffer);
-	if (sttc_str && *sttc_str)
-		return (sttc_str);
-	return (NULL);
-}
 char	*get_next_line(int fd)
 {
 	static char	*sttc_str;
 	char		*to_ret;
-	char		*temp;
 
 	if (BUFFER_SIZE < 1 || fd < 0)
 		return (NULL);
 	sttc_str = get_line(fd, sttc_str);
 	if (!sttc_str)
 		return (NULL);
-	else if (g_check_n(sttc_str) >= 0)
-	{
-		to_ret = g_cut_to_n(sttc_str);
-		temp = g_cut_from_n(sttc_str);
-		free(sttc_str);
-		sttc_str = temp;
-	}
-	else
-		return (sttc_str);
-//		to_ret = g_strdup(sttc_str);
-	return (to_ret);
 }
-/*int	main(void)
-{
-//	char *str1;
-//	str1 = g_calloc(1);
-	char *str2;
-	str2 = g_calloc(6);
-	str2[0] = 'X';
-	str2[1] = 'A';
-	str2[2] = '\n';
-	str2[3] = 'A';
-	str2[4] = 'X';
-	str2[5] = 0;
-	str2 = g_cut_to_n(str2);
-	str2 = g_cat(str2, str2, 3);
-	printf("%s", str2);
-}*/
